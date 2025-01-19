6c18a619d29fafa433d0181aa38433b7ee644b41
ShaikAbbuSofiyan <sksofiyan004@gmail.com>
2025-01-19 21:34:41 +0530

    :main and vite config changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode >
+  // </StrictMode>,
+  <div>
     <App />
-  </StrictMode>,
+
+  </div>
 )
