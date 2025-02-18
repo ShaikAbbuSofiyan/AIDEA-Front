f6a85515d49ea3f938acaba1150d925aee269984
ShaikAbbuSofiyan <sksofiyan004@gmail.com>
2025-02-18 18:38:05 +0530

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:"/AIDEA-Front/",
+  base:"/AIDEA-Front",
   plugins: [react()],
 })
